82793e46a77204819c4dfd8dc991fbf5ecd8d8ba
Eve Stroud <evestroud89@gmail.com>
2023-01-06 08:17:49 -0500

    Add code for exercises 1.6 & 1.7

--- chapter-1/1-1.scm
+++ chapter-1/1-1.scm
@@ -1,9 +1,9 @@
 ;; 1.1.7 Example: Square Roots by Newton’s Method
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt-iter guess prev-guess x)
+  (if (good-enough? guess prev-guess x)
       guess
-      (sqrt-iter (improve guess x) x)))
+      (sqrt-iter (improve guess x) guess x)))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -11,8 +11,20 @@
 (define (average . items)
   (/ (apply + items) (length items)))
 
-(define (good-enough? guess x)
-  (< (abs (- (* guess guess) x)) .000001))
+; Exercise 1.7
+
+;; (define (good-enough? guess prev-guess x)
+;;   (< (abs (- (* guess guess) x)) .001))
+
+(define (good-enough? guess prev-guess x)
+   (< (/ (abs (- guess prev-guess)) guess) 0.001))
 
 (define (sqrt x)
-  (sqrt-iter (average 1 (/ x 2.0)) x)) ; 2.0 to force a float
+  (sqrt-iter 1.0 (inf) x)) ; 2.0 to force a float
+
+;; Exercise 1.6
+(define (new-if predicate
+                then-clause
+                else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
