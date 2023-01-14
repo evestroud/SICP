8a734aa3f43469d7d056a68ee714e9614894b8bf
Eve Stroud <evestroud89@gmail.com>
2023-01-14 10:46:58 -0500

    Finish exercise 1.19

--- chapter-1/1-2.scm
+++ chapter-1/1-2.scm
@@ -153,3 +153,26 @@
      ((even? a) (iter (halve a) (double b) c))
      (else (iter (- a 1) b (+ b c)))))
   (iter a b 0))
+
+;; Exercise 1.19
+
+(define (fib n)
+  (define (iter a b p q count)
+    (cond ((= count 0)
+           b)
+          ((even? count)
+           (iter a
+                 b
+                 (+ (square p) (square q))
+                 (+ (* 2 p q) (square q))
+                 (/ count 2)))
+          (else
+           (iter (+ (* b q)
+                    (* a q)
+                    (* a p))
+                 (+ (* b p)
+                    (* a q))
+                 p
+                 q
+                 (- count 1)))))
+  (iter 1 0 0 1 n))
