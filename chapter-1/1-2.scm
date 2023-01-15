7a4cd4d8cf77a1918bd6ccb114b116a420dadfb0
Eve Stroud <evestroud89@gmail.com>
2023-01-15 16:15:58 -0500

    Add code for sections 1.2.5 & 1.2.6

--- chapter-1/1-2.scm
+++ chapter-1/1-2.scm
@@ -176,3 +176,53 @@
                  q
                  (- count 1)))))
   (iter 1 0 0 1 n))
+
+
+;; 1.2.5 Greatest Common Divisors
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+
+;; 1.2.6 Testing for Primality
+
+(define (smallest-divisor n)
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n)
+           n)
+          ((divides? test-divisor n)
+           test-divisor)
+          (else (find-divisor
+                 n
+                 (+ test-divisor 1)))))
+  (find-divisor n 2))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n)
+         (fast-prime? n (- times 1)))
+        (else #f)))
