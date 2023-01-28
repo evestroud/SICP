eba73b8207ee84c36307e4ada857535b25278905
Eve Stroud <evestroud89@gmail.com>
2023-01-28 08:09:40 -0500

    Finish exercise 1.31

--- chapter-1/1-3.scm
+++ chapter-1/1-3.scm
@@ -52,3 +52,25 @@
         result
         (iter (next a) (+ (term a) result))))
   (iter a 0))
+
+;; Exercise 1.31
+
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(define (product-recursive term a next b)
+  (if (> a b) 1
+      (* (term a) (product-recursive term (next a) next b))))
+
+(define (factorial n)
+  (product-recursive identity 1 1+ n))
+
+(define (wallis-approx precision)
+  (define (term n)
+    (* (/ (* 2 n) (- (* 2 n) 1))
+       (/ (* 2 n) (+ (* 2 n) 1))))
+  (* 2 (product term 1 1+ precision)))
