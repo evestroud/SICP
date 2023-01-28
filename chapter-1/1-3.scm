c7446ebaaec0a4b81b00a2cef08bed024fae32b9
Eve Stroud <evestroud89@gmail.com>
2023-01-28 08:21:04 -0500

    Finish exercise 1.32

--- chapter-1/1-3.scm
+++ chapter-1/1-3.scm
@@ -74,3 +74,30 @@
     (* (/ (* 2 n) (- (* 2 n) 1))
        (/ (* 2 n) (+ (* 2 n) 1))))
   (* 2 (product term 1 1+ precision)))
+
+;; 1.32
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+(define (accumulate-recursive combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate-recursive
+                 combiner
+                 null-value
+                 term
+                 (next a)
+                 next
+                 b))))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate-recursive * 1 term a next b))
