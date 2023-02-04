6b952ebb6266400dbab1e1e703724847e3708c45
Eve Stroud <evestroud89@gmail.com>
2023-02-03 19:46:57 -0500

    Finish exercises 1.35 & 1.36

--- chapter-1/1-3.scm
+++ chapter-1/1-3.scm
@@ -182,17 +182,31 @@
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
+  ;; (define count 0) ; Exercise 1.36
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2))
        tolerance))
   (define (try guess)
+    ;; (set! count (1+ count)) ; Exercise 1.36
+    ;; (format #t "~a\n" guess) ; Exercise 1.36
     (let ((next (f guess)))
       (if (close-enough? guess next)
           next
-          (try next))))
-  (try first-guess))
+          (try (average guess next)))))
+  ;; (cons ; Exercise 1.36
+   (try first-guess)
+        ;; count) ; Exercise 1.36
+  )
 
 (define (sqrt x)
   (fixed-point
    (lambda (y) (average y (/ x y)))
    1.0))
+
+;; Exercise 1.35
+
+;(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0); -> 1.618...
+
+;; Exercise 1.36
+
+;(fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
