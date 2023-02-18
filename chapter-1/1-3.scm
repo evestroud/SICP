eb7a8352647e8c1f526c983eaa0fb85f87205470
Eve Stroud <evestroud89@gmail.com>
2023-02-18 07:26:30 -0500

    Add code for section 1.3.4

--- chapter-1/1-3.scm
+++ chapter-1/1-3.scm
@@ -249,3 +249,60 @@
               (lambda (i) (if (= 1 i) x (square x)))
               (lambda (i) (+ 1 (* 2 (- i 1))))
               k)))
+
+;; 1.3.4
+
+(define (average-damp f)
+  (lambda (x)
+    (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point
+   (average-damp
+    (lambda (y) (/ x y)))
+   1.0))
+
+(define (cube-root x)
+  (fixed-point
+   (average-damp
+    (lambda (y)
+      (/ x (square y))))
+   1.0))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x)
+            ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g)
+               guess))
+
+(define (sqrt x)
+  (newtons-method
+   (lambda (y)
+     (- (square y) x))
+   1.0))
+
+(define (fixed-point-of-transform
+         g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt x)
+  (fixed-point-of-transform
+   (lambda (y) (/ x y))
+   average-damp
+   1.0))
+
+(define (sqrt x)
+  (fixed-point-of-transform
+   (lambda (y) (- (square y) x))
+   newton-transform
+   1.0))
