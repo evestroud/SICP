59ddb24fac26d5ac0975ffd307c5c6608ac506f5
Eve Stroud <evestroud89@gmail.com>
2023-03-13 21:07:35 -0400

    Add code for 2.1.1 and exercise 2.1

---
+++ chapter-1/2-1.scm
@@ -0,0 +1,42 @@
+;; 2.1.1 Arithmetic Operations for Rational Numbers
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define one-half (make-rat 1 2))
+(define one-third (make-rat 1 3))
+
+;; Exercise 2.1
+
+;; (define (make-rat n d)
+;;   )
