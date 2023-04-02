9bc9cee37a63e68ac3ecba81a3c9c88bda63ad02
Eve Stroud <evestroud89@gmail.com>
2023-04-01 22:50:10 -0400

    Finish exercise 2.19

--- chapter-2/2-2.scm
+++ chapter-2/2-2.scm
@@ -17,3 +17,22 @@
         (helper (cdr l)
                 (append (list (car l)) r))))
   (helper l '()))
+
+
+;; Exercise 2.19
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0)
+             (null? kinds-of-coins))
+         0)
+        (else
+         (+ (cc amount (cdr kinds-of-coins))
+            (cc (- amount (car kinds-of-coins))
+                kinds-of-coins)))))
+
+(define us-coins
+  (list 50 25 10 5 1))
+
+(define uk-coins
+  (list 100 50 20 10 5 2 1 0.5))
