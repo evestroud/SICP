a4cf3df9ac2954f509825c520282ae1b3a85f481
Eve Stroud <evestroud89@gmail.com>
2023-04-09 13:50:37 -0400

    Add code for Nested Mappings and finish exercise 2.40

--- chapter-2/2-2.scm
+++ chapter-2/2-2.scm
@@ -317,3 +317,47 @@
 (define (reverse sequence)
   (fold-left
    (lambda (x y) (cons y x)) nil sequence))
+
+
+;; Nested Mappings
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair)
+        (cadr pair)
+        (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter
+        prime-sum?
+        (flatmap
+         (lambda (i)
+           (map (lambda (j)
+                  (list i j))
+                (enumerate-interval
+                 1
+                 (- i 1))))
+         (enumerate-interval 1 n)))))
+
+
+;; Exercise 2.40
+
+(define (unique-pairs n)
+  (flatmap
+   (lambda (i)
+     (map (lambda (j)
+            (list i j))
+          (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter
+        prime-sum?
+        (unique-pairs n))))
