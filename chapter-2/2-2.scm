f1dbcd55929f0a33152cea2b9f03b51b1fd3fb6a
Eve Stroud <evestroud89@gmail.com>
2023-04-09 07:38:17 -0400

    Comment out redefinitions from 2.33

--- chapter-2/2-2.scm
+++ chapter-2/2-2.scm
@@ -231,16 +231,16 @@
 
 ;; Exercise 2.33
 
-(define (map p sequence)
-  (accumulate (lambda (x y) (cons (p x) y))
-              '() sequence))
+;; (define (map p sequence)
+;;   (accumulate (lambda (x y) (cons (p x) y))
+;;               '() sequence))
 
-(define (append seq1 seq2)
-  (accumulate cons seq2 seq1))
+;; (define (append seq1 seq2)
+;;   (accumulate cons seq2 seq1))
 
-(define (length sequence)
-  (accumulate (lambda (x y) (1+ y))
-              0 sequence))
+;; (define (length sequence)
+;;   (accumulate (lambda (x y) (1+ y))
+;;               0 sequence))
 
 
 ;; Exercise 2.34
