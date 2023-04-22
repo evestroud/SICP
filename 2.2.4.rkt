2baf063f37f6d9ea198a584b622336f5e8aea5ff
Eve Stroud <evestroud89@gmail.com>
2023-04-22 08:44:06 -0400

    Finish exercises 2.46 & 2.47

--- 2.2.4.rkt
+++ 2.2.4.rkt
@@ -100,3 +100,74 @@
 (define right-split-2 (split beside below))
 
 (define up-split-2 (split below beside))
+
+
+;; Frames
+
+;; Exercise 2.46
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vector (+ (xcor-vect v1)
+                  (xcor-vect v2))
+               (+ (ycor-vect v1)
+                  (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vector (- (xcor-vect v1)
+                  (xcor-vect v2))
+               (- (ycor-vect v1)
+                  (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+
+;; Exercise 2.47
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (caddr frame))
+
+;; (define (make-frame origin edge1 edge2)
+;;   (cons origin (cons edge1 edge2)))
+
+;; (define (origin-frame frame)
+;;   (car frame))
+
+;; (define (edge1-frame frame)
+;;   (cadr frame))
+
+;; (define (edge2-frame frame)
+;;   (cddr frame))
+
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect
+      (scale-vect (xcor-vect v)
+                  (edge1-frame frame))
+      (scale-vect (ycor-vect v)
+                  (edge2-frame frame))))))
+
+
+;; Painters
