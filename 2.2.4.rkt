a507f68af15e3d10809a4f415722c838b0804fdd
Eve Stroud <evestroud89@gmail.com>
2023-04-23 09:32:53 -0400

    Comment out redefinitions of library fns

--- 2.2.4.rkt
+++ 2.2.4.rkt
@@ -106,45 +106,45 @@
 
 ;; Exercise 2.46
 
-(define (make-vect x y)
-  (cons x y))
+;; (define (make-vect x y)
+;;   (cons x y))
 
-(define (xcor-vect v)
-  (car v))
+;; (define (xcor-vect v)
+;;   (car v))
 
-(define (ycor-vect v)
-  (cdr v))
+;; (define (ycor-vect v)
+;;   (cdr v))
 
-(define (add-vect v1 v2)
-  (make-vector (+ (xcor-vect v1)
-                  (xcor-vect v2))
-               (+ (ycor-vect v1)
-                  (ycor-vect v2))))
+;; (define (add-vect v1 v2)
+;;   (make-vector (+ (xcor-vect v1)
+;;                   (xcor-vect v2))
+;;                (+ (ycor-vect v1)
+;;                   (ycor-vect v2))))
 
-(define (sub-vect v1 v2)
-  (make-vector (- (xcor-vect v1)
-                  (xcor-vect v2))
-               (- (ycor-vect v1)
-                  (ycor-vect v2))))
+;; (define (sub-vect v1 v2)
+;;   (make-vector (- (xcor-vect v1)
+;;                   (xcor-vect v2))
+;;                (- (ycor-vect v1)
+;;                   (ycor-vect v2))))
 
-(define (scale-vect s v)
-  (make-vect (* s (xcor-vect v))
-             (* s (ycor-vect v))))
+;; (define (scale-vect s v)
+;;   (make-vect (* s (xcor-vect v))
+;;              (* s (ycor-vect v))))
 
 
 ;; Exercise 2.47
 
-(define (make-frame origin edge1 edge2)
-  (list origin edge1 edge2))
+;; (define (make-frame origin edge1 edge2)
+;;   (list origin edge1 edge2))
 
-(define (origin-frame frame)
-  (car frame))
+;; (define (origin-frame frame)
+;;   (car frame))
 
-(define (edge1-frame frame)
-  (cadr frame))
+;; (define (edge1-frame frame)
+;;   (cadr frame))
 
-(define (edge2-frame frame)
-  (caddr frame))
+;; (define (edge2-frame frame)
+;;   (caddr frame))
 
 ;; (define (make-frame origin edge1 edge2)
 ;;   (cons origin (cons edge1 edge2)))
@@ -159,15 +159,15 @@
 ;;   (cddr frame))
 
 
-(define (frame-coord-map frame)
-  (lambda (v)
-    (add-vect
-     (origin-frame frame)
-     (add-vect
-      (scale-vect (xcor-vect v)
-                  (edge1-frame frame))
-      (scale-vect (ycor-vect v)
-                  (edge2-frame frame))))))
+;; (define (frame-coord-map frame)
+;;   (lambda (v)
+;;     (add-vect
+;;      (origin-frame frame)
+;;      (add-vect
+;;       (scale-vect (xcor-vect v)
+;;                   (edge1-frame frame))
+;;       (scale-vect (ycor-vect v)
+;;                   (edge2-frame frame))))))
 
 
 ;; Painters
