7da687fb9a6e070ce4ffecf3e717d1808ef97275
Eve Stroud <evestroud89@gmail.com>
2023-09-02 12:27:25 -0400

    Add notes and code for Message Passing

--- 2.4.scm
+++ 2.4.scm
@@ -387,3 +387,18 @@
     (lambda (division)
       ((get 'employee-record division) employee-id))
     all-divisions)))
+
+
+;; Message passing
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+           (error "Unknown op:
+            MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
