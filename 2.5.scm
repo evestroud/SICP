eebcf893f11f0340a3cf5c1fdfa73a88330c838b
Eve Stroud <evestroud89@gmail.com>
2023-11-09 17:09:03 -0500

    partial implementation of exercise ?

--- 2.5.scm
+++ 2.5.scm
@@ -259,20 +259,20 @@
   ;; internal procedures
   (define (add-complex z1 z2)
     (make-from-real-imag
-     (+ (real-part z1) (real-part z2))
-     (+ (imag-part z1) (imag-part z2))))
+     (add (real-part z1) (real-part z2))
+     (add (imag-part z1) (imag-part z2))))
   (define (sub-complex z1 z2)
     (make-from-real-imag
-     (- (real-part z1) (real-part z2))
-     (- (imag-part z1) (imag-part z2))))
+     (sub (real-part z1) (real-part z2))
+     (sub (imag-part z1) (imag-part z2))))
   (define (mul-complex z1 z2)
     (make-from-mag-ang
-     (* (magnitude z1) (magnitude z2))
-     (+ (angle z1) (angle z2))))
+     (mul (magnitude z1) (magnitude z2))
+     (add (angle z1) (angle z2))))
   (define (div-complex z1 z2)
     (make-from-mag-ang
-     (/ (magnitude z1) (magnitude z2))
-     (- (angle z1) (angle z2))))
+     (div (magnitude z1) (magnitude z2))
+     (sub (angle z1) (angle z2))))
   ;; interface to rest of the system
   (define (tag z) (attach-tag 'complex z))
   (put 'add '(complex complex)
@@ -478,6 +478,7 @@
     (proc x)))
 
 (define (apply-binary op x y)
+  (println op x y)
   (let* ((x-type (type-tag x))
          (y-type (type-tag y))
          (proc (get op (list x-type y-type))))
